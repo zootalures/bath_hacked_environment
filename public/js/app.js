a8f04ea203c9de51a82e32f79f51c1a2744fb743
Owen Cliffe <owen.cliffe@oracle.com>
2016-02-14 14:11:38 +0000

    adding new challenge

--- public/js/app.js
+++ public/js/app.js
@@ -10,11 +10,12 @@ var app = angular.module('beastie',['ngRoute','leaflet-directive','chart.js','ng
             .when('/ward/:ward_id/', {templateUrl: 'partials/ward.html', controller: WardController})
 
             .when('/ward/:ward_id/challenges/tweet_in_the_park', {templateUrl: 'partials/challenge_tweet_in_the_park.html', controller: ChallengeController})
-            .when('/ward/:ward_id/challenges/noisy_neighbors', {templateUrl: 'partials/new_challenge_noisy_neighbors.html', controller: ChallengeController})
+            .when('/ward/:ward_id/challenges/noisy_neighbors', {templateUrl: 'partials/new_challenge_noisy_neighbors.html', controller: NewChallengeController})
             .otherwise({redirectTo: '/'});;
     }])
     .controller(WardController)
     .controller(HomeController)
+    .controller(NewChallengeController)
     .directive('enviromate',Enviromate);
 
 
