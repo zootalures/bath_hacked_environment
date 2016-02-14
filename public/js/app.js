629a0ac52476a3782368d01505507cadf2b1684a
Owen Cliffe <owen.cliffe@oracle.com>
2016-02-14 11:18:49 +0000

    adding tweets to challenges

--- public/js/app.js
+++ public/js/app.js
@@ -3,11 +3,13 @@
  */
 
 
-var app = angular.module('beastie',['ngRoute','leaflet-directive','chart.js'])
+var app = angular.module('beastie',['ngRoute','leaflet-directive','chart.js','ngtweet'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider
             .when('/', {templateUrl: 'partials/home.html', controller:HomeController})
             .when('/ward/:ward_id/', {templateUrl: 'partials/ward.html', controller: WardController})
+
+            .when('/ward/:ward_id/challenges/tweet_in_the_park', {templateUrl: 'partials/challenge_tweet_in_the_park.html', controller: ChallengeController})
             .otherwise({redirectTo: '/'});;
     }])
     .controller(WardController)
